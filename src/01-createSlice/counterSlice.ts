e77b4835d75bf9e3e64ab08f920211febf8fae9d
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-01-04 12:00:31 +0700

    remove opinionated files

--- src/01-createSlice/counterSlice.ts
+++ src/01-createSlice/counterSlice.ts
@@ -1,15 +1,1 @@
-import { createSlice } from "@reduxjs/toolkit";
 
-export type CounterState = {
-  value: number;
-};
-
-const initialState: CounterState = {
-  value: 0,
-};
-
-export const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {}, // Implement your reducers here
-});
